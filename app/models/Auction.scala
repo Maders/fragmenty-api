4a7c75ea829b85b5d1d4cde3fd365cb745393c73
Ehsan Titish <Maders@users.noreply.github.com>
2023-04-04 02:07:26 +0330

    chore: format the code

--- app/models/Auction.scala
+++ app/models/Auction.scala
@@ -1,26 +1,17 @@
 package models
 
 import play.api.libs.json._
-//import java.math.BigDecimal
 
 case class Auction(
-                    _id: String,
-                    id: String,
-                    auctionEndTimestamp: String,
-                    memorabilityScore: Double,
-                    minimumBid: Int,
-                    minimumBidInUSD: Int,
-                    number: String
-                  )
+    _id: String,
+    id: String,
+    auctionEndTimestamp: String,
+    memorabilityScore: Double,
+    minimumBid: Int,
+    minimumBidInUSD: Int,
+    number: String
+)
 
 object Auction {
-//  implicit val decimal128Reads: Reads[Decimal128] = Reads { json =>
-//    json.validate[String].map(s => new BigDecimal(s)).map(new Decimal128(_))
-//  }
-//
-//  implicit val decimal128Writes: Writes[Decimal128] = Writes { decimal128 =>
-//    JsString(decimal128.toString)
-//  }
-
   implicit val auctionFormat: Format[Auction] = Json.format[Auction]
 }
