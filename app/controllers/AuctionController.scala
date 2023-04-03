5486d9408e94ef5ed631e089eba66d2a0cdea3a1
Ehsan Titish <Maders@users.noreply.github.com>
2023-04-04 02:07:26 +0330

    feat: web socket endpoint always send data every one minute

--- app/controllers/AuctionController.scala
+++ app/controllers/AuctionController.scala
@@ -6,21 +6,22 @@ import akka.stream.scaladsl._
 import play.api.libs.json._
 import play.api.mvc._
 import repositories._
+import akka.stream.scaladsl.Source
+import scala.concurrent.duration._
 
 import javax.inject.{Inject, Singleton}
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext
 
 @Singleton
 class AuctionController @Inject() (
-                                 val controllerComponents: ControllerComponents,
-                                 auctionRepository: AuctionRepository
-                               )(implicit ec: ExecutionContext, system: ActorSystem, mat: Materializer)
-  extends BaseController {
+    val controllerComponents: ControllerComponents,
+    auctionRepository: AuctionRepository
+)(implicit ec: ExecutionContext, system: ActorSystem, mat: Materializer)
+    extends BaseController {
 
   def authenticateUser: Action[AnyContent] = Action { implicit request =>
     Ok("User authenticated").withSession("user" -> "testuser")
   }
-
 
   def getAuctions(page: Int, pageSize: Int): Action[AnyContent] = Action.async {
     implicit request: Request[AnyContent] =>
@@ -30,23 +31,22 @@ class AuctionController @Inject() (
       }
   }
 
-  def auctionsWebSocket: WebSocket =
-    WebSocket.acceptOrResult[JsValue, JsValue] { request =>
-      Future.successful(request.session.get("user") match {
-        case None =>
-          Left(Forbidden)
-        case Some(_) =>
-          val source = Source
+  def auctionsWebSocket: WebSocket = {
+    WebSocket.accept[JsValue, JsValue] { request =>
+      val source = Source
+        .tick(0.seconds, 1.minute, ())
+        .flatMapConcat { _ =>
+          Source
             .fromPublisher(auctionRepository.findAllPublisher(10, 0))
             .map(Json.toJson(_))
             .grouped(10)
             .map(
               _.foldLeft(JsArray())((acc, jsValue) => acc ++ Json.arr(jsValue))
             )
+        }
 
-          val flow = Flow.fromSinkAndSource(Sink.ignore, source)
-
-          Right(flow)
-      })
+      val flow = Flow.fromSinkAndSource(Sink.ignore, source)
+      flow
     }
+  }
 }
